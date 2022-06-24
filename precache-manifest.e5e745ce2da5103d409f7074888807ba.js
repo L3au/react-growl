7aca5cd8c96bfd2189ce5aae105a9ee00afc8b68
Ben Liu <leshu.lau@gmail.com>
2022-06-25 11:32:33 +1200

    Updates

---
+++ precache-manifest.e5e745ce2da5103d409f7074888807ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42377859fbf6412b9c2304489677fb93",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f2106cd7981b814d58f7",
+    "url": "./static/css/main.7e386e7d.chunk.css"
+  },
+  {
+    "revision": "8b2389e2018367a36fdc",
+    "url": "./static/js/2.5c1c43ce.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.5c1c43ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2106cd7981b814d58f7",
+    "url": "./static/js/main.329710c5.chunk.js"
+  },
+  {
+    "revision": "d7cb6fe57c5515cac0a8",
+    "url": "./static/js/runtime-main.f7eda834.js"
+  },
+  {
+    "revision": "aab9f604e92e57a3b0dffd2633384aef",
+    "url": "./static/media/close-white.aab9f604.svg"
+  }
+]);
